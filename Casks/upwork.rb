5fa2b1c931938c4da5561ed04cf10d00570e27c7
Nick Kadutskyi <nick@kadutskyi.com>
2024-12-20 01:39:57 -0800

    Adds livecheck for upwork

--- Casks/upwork.rb
+++ Casks/upwork.rb
@@ -1,25 +1,36 @@
 cask "upwork" do
     arch arm: "arm64", intel: "x64"
-    version "5.8.0.35_be1a1520901c4eef"
+    version "5.8.0.35,be1a1520901c4eef"
     sha256 arm: "19a455b6d97d239a1f914f075f365c199077d2ac3e90a0658b3a3a45c6a450f5",
            intel: "19a455b6d97d239a1f914f075f365c199077d2ac3e90a0658b3a3a45c6a450f5"
   
-    url "https://upwork-usw2-desktopapp.upwork.com/binaries/v#{version.dots_to_underscores}/Upwork.dmg",
+    url "https://upwork-usw2-desktopapp.upwork.com/binaries/v#{version.csv.first.dots_to_underscores}_#{version.csv.second}/Upwork.dmg",
         user_agent: :fake,
         referer: "https://www.upwork.com/ab/downloads"
     name "Upwork"
     desc "Desktop app for Upwork"
     homepage "https://www.upwork.com/"
-    auto_updates true
+    
     livecheck do
-      url :url
+        url "https://upwork-usw2-desktopapp.upwork.com/binaries/versions-mac.json"
+        strategy :page_match do |page|
+          match = page.match(%r{/v(\d+(?:_\d+)*)_([^/]+)/Upwork\.dmg}i)
+          next if match.blank?
+    
+          "#{match[1].tr("_", ".")},#{match[2]}"
+        end
     end
+    
+    auto_updates true
   
     app "Upwork.app"
-  
+
     zap trash: [
-      "~/Library/Application Support/Upwork",
-      "~/Library/Preferences/com.upwork.Upwork.plist",
-      "~/Library/Saved Application State/com.upwork.Upwork.savedState"
+        "~/Library/Application Support/CrashReporter/Upwork_*",
+        "~/Library/Application Support/Upwork",
+        "~/Library/Caches/Upwork",
+        "~/Library/Logs/Upwork",
+        "~/Library/Preferences/com.upwork.Upwork.plist",
+        "~/Library/Saved Application State/com.upwork.upwork.savedState",
     ]
-  end  
+  end
