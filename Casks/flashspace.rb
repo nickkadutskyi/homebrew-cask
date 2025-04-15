81b018a50e318800443817c9cd8c7a3356e7f101
Nick Kadutskyi <nick@kadutskyi.com>
2025-04-15 16:26:35 -0700

    feat(casks): adds Flashspace cask with livecheck

---
+++ Casks/flashspace.rb
@@ -0,0 +1,31 @@
+cask "flashspace" do
+  version "3.1.37"
+  sha256 "aba30fbd596fa95ebd977c28775247ef5a7e735dd5149f14611c701416a8ee1b"
+
+  url "https://github.com/wojciech-kulik/FlashSpace/releases/download/v#{version}/FlashSpace.app.zip"
+  name "FlashSpace"
+  desc "Virtual workspace manager"
+  homepage "https://github.com/wojciech-kulik/FlashSpace"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+    regex(/v?(\d+\.\d+\.\d+)/i)
+  end
+
+  depends_on macos: ">= :sonoma"
+
+  app "FlashSpace.app"
+
+  uninstall quit: "pl.wojciechkulik.FlashSpace"
+
+  zap trash: [
+    "~/Library/Application Scripts/pl.wojciechkulik.FlashSpace",
+    "~/Library/Autosave Information/pl.wojciechkulik.FlashSpace.plist",
+    "~/Library/Caches/pl.wojciechkulik.FlashSpace",
+    "~/Library/HTTPStorages/pl.wojciechkulik.FlashSpace",
+    "~/Library/Preferences/FlashSpace.plist",
+    "~/Library/Preferences/pl.wojciechkulik.FlashSpace.plist",
+    "~/Library/Saved Application State/pl.wojciechkulik.FlashSpace.savedState"
+  ]
+end
